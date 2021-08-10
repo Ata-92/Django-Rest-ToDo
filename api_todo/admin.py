8b07c89d873c4cf503c2ebd5c7779b7453ea5e3b
Ata-92 <atakal92@gmail.com>
2021-08-10 21:58:37 +0300

    Add the model to admin page and customize it

--- api_todo/admin.py
+++ api_todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from api_todo.models import Todo
 
 # Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ["task", "description", "priority", "done"]
+
+admin.site.register(Todo, TodoAdmin)
