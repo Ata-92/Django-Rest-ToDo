2ade4f169932a03d247b1e653677c70ce33a3109
Ata-92 <atakal92@gmail.com>
2021-08-10 21:55:34 +0300

    Add a model

--- api_todo/models.py
+++ api_todo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    task = models.CharField(max_length=50)
+    description = models.TextField()
+    done = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    TITLES = [
+        ("H", "High"),
+        ("M", "Medium"),
+        ("L", "Low")
+    ]
+    priority = models.CharField(max_length=50, choices=TITLES)
+
+    def __str__(self):
+        return self.task
