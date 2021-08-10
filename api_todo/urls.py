10c28a155675eca9d6a17021afe856ddb33167d0
Ata-92 <atakal92@gmail.com>
2021-08-10 22:59:38 +0300

    Add get and post views

--- api_todo/urls.py
+++ api_todo/urls.py
@@ -1,6 +1,9 @@
-from api_todo.views import home_view
+from api_todo.views import home_view, todocreate_view, todolist_view, todolistcreate_view
 from django.urls import path
 
 urlpatterns = [
-    path("", home_view)
+    path("", home_view),
+    path("todolist/", todolist_view),
+    path("todocreate/", todocreate_view),
+    path("todolistcreate/", todolistcreate_view),
 ]
--- api_todo/views.py
+++ api_todo/views.py
@@ -1,7 +1,38 @@
+from api_todo.models import Todo
+from api_todo.serializers import TodoSerializer
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
 def home_view(request):
     return HttpResponse("""<center><h1 style="background-color: aqua;">Welcome to ToDo App</h1></center>""")
+
+@api_view(["GET"])
+def todolist_view(request):
+    queryset = Todo.objects.all()
+    serializer = TodoSerializer(queryset, many=True)
+    return Response(serializer.data)
+
+@api_view(["POST"])
+def todocreate_view(request):
+    serializer = TodoSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(["GET", "POST"])
+def todolistcreate_view(request):
+    if request.method == "GET":
+        queryset = Todo.objects.all()
+        serializer = TodoSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
